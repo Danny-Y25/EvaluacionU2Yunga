bc62b153ec22c35896ede842da8136b894629265
Danny-Y25 <69018308+Danny-Y25@users.noreply.github.com>
2021-12-14 14:38:52 -0500

    Commit de Recuperacion

--- ClienteVehiculo/src/main/java/ec/edu/ups/vista/Ventana.java
+++ ClienteVehiculo/src/main/java/ec/edu/ups/vista/Ventana.java
@@ -18,8 +18,13 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JSeparator;
+import java.awt.Color;
 
 public class Ventana extends JFrame {
 
@@ -30,6 +35,10 @@ public class Ventana extends JFrame {
 	private JTextField textField_3;
 	
 	private VehiculoONRemote vehRemote;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
 
 	/**
 	 * Launch the application.
@@ -53,7 +62,7 @@ public class Ventana extends JFrame {
 	 */
 	public Ventana() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 639, 320);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
@@ -64,22 +73,22 @@ public class Ventana extends JFrame {
 		panel.setLayout(null);
 		
 		textField = new JTextField();
-		textField.setBounds(179, 48, 86, 20);
+		textField.setBounds(145, 48, 86, 20);
 		panel.add(textField);
 		textField.setColumns(10);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(179, 85, 86, 20);
+		textField_1.setBounds(145, 85, 86, 20);
 		panel.add(textField_1);
 		textField_1.setColumns(10);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(179, 135, 86, 20);
+		textField_2.setBounds(145, 135, 86, 20);
 		panel.add(textField_2);
 		textField_2.setColumns(10);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(179, 177, 86, 20);
+		textField_3.setBounds(145, 177, 86, 20);
 		panel.add(textField_3);
 		textField_3.setColumns(10);
 		
@@ -90,28 +99,87 @@ public class Ventana extends JFrame {
 				datosProducto();
 			}
 		});
-		btnNewButton.setBounds(299, 109, 89, 23);
+		btnNewButton.setBounds(110, 221, 89, 23);
 		panel.add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("Vehiculo");
-		lblNewLabel.setBounds(179, 11, 46, 14);
+		JLabel lblNewLabel = new JLabel("Ingresar Vehiculo");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(79, 17, 151, 20);
 		panel.add(lblNewLabel);
 		
 		JLabel lblTipo = new JLabel("Tipo");
-		lblTipo.setBounds(44, 138, 46, 14);
+		lblTipo.setBounds(10, 138, 46, 14);
 		panel.add(lblTipo);
 		
 		JLabel lblAo = new JLabel("A\u00F1o");
-		lblAo.setBounds(44, 180, 46, 14);
+		lblAo.setBounds(10, 180, 46, 14);
 		panel.add(lblAo);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Modelo");
-		lblNewLabel_1_1.setBounds(44, 88, 46, 14);
+		lblNewLabel_1_1.setBounds(10, 88, 46, 14);
 		panel.add(lblNewLabel_1_1);
 		
 		JLabel lblNewLabel_1_2 = new JLabel("Placa");
-		lblNewLabel_1_2.setBounds(44, 51, 46, 14);
+		lblNewLabel_1_2.setBounds(10, 51, 46, 14);
 		panel.add(lblNewLabel_1_2);
+		
+		JLabel lblNewLabel_1 = new JLabel("Editar Vehiculo");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(369, 17, 113, 20);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Placa");
+		lblNewLabel_2.setBounds(369, 51, 46, 14);
+		panel.add(lblNewLabel_2);
+		
+		textField_4 = new JTextField();
+		textField_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				consultarCliente();
+				
+			}
+		});
+		textField_4.setBounds(414, 48, 86, 20);
+		panel.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("Actualizar");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				actualizarProducto();
+				
+			}
+		});
+		btnNewButton_1.setBounds(390, 221, 113, 23);
+		panel.add(btnNewButton_1);
+		
+		textField_5 = new JTextField();
+		textField_5.setBounds(331, 101, 86, 20);
+		panel.add(textField_5);
+		textField_5.setColumns(10);
+		
+		textField_6 = new JTextField();
+		textField_6.setBounds(502, 101, 86, 20);
+		panel.add(textField_6);
+		textField_6.setColumns(10);
+		
+		textField_7 = new JTextField();
+		textField_7.setBounds(414, 148, 86, 20);
+		panel.add(textField_7);
+		textField_7.setColumns(10);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Modelo");
+		lblNewLabel_1_1_1.setBounds(275, 104, 46, 14);
+		panel.add(lblNewLabel_1_1_1);
+		
+		JLabel lblTipo_1 = new JLabel("Tipo");
+		lblTipo_1.setBounds(446, 107, 46, 14);
+		panel.add(lblTipo_1);
+		
+		JLabel lblAo_1 = new JLabel("A\u00F1o");
+		lblAo_1.setBounds(358, 151, 46, 14);
+		panel.add(lblAo_1);
 	}
 	
 	
@@ -159,21 +227,44 @@ public void datosProducto() {
 		
 		try {
 			this.vehRemote.insertar(veh);
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		    
-		
+		JOptionPane.showMessageDialog(null, "Vehiculo Agregado");   
+		Limpiar1();
 	}
+
+protected void consultarCliente() {
+	// TODO Auto-generated method stub
+	String placa=textField_4.getText();
+	Vehiculo p = this.vehRemote.getDatos(placa);
+	
+	if (p==null) {
+		textField_5.setText("");
+		textField_6.setText("");
+		textField_7.setText("");
+		JOptionPane.showMessageDialog(null, "Vehiculo no registrado");
+		
+		
+	}else {
+		textField_5.setText(p.getModelo());
+		textField_6.setText(p.getTipo());
+		textField_7.setText(p.getAnio());
+	}
+	
+	
+}
+
 
 public void actualizarProducto() {
 	
-	String placa=textField.getText();
-	String modelo=textField_1.getText();
-	String tipo=textField_2.getText();
-	String anio =textField_3.getText();
+	String placa=textField_4.getText();
+	String modelo=textField_5.getText();
+	String tipo=textField_6.getText();
+	String anio =textField_7.getText();
 	
 	Vehiculo veh=new Vehiculo();
 	veh.setPlaca(placa);
@@ -184,13 +275,28 @@ public void actualizarProducto() {
 	
 	
 	try {
-		this.vehRemote.insertar(veh);
+		this.vehRemote.actualizar(veh);;
 	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
+	JOptionPane.showMessageDialog(null, "Vehiculo Actualizado");
+	Limpiar();
+	  
 	
-	    
-	
+}
+
+public void Limpiar(){
+	textField_4.setText("");
+	textField_5.setText("");
+	textField_6.setText("");
+	textField_7.setText("");
+}
+
+public void Limpiar1(){
+	textField.setText("");
+	textField_1.setText("");
+	textField_2.setText("");
+	textField_3.setText("");
 }
 }
